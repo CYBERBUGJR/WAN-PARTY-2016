429aa297efa970c4aa8ecebcf7020249858d9114
root <root@localhost.localdomain>
2016-10-22 21:50:54 +0200

    Correction Erreur

--- ntopng.sh
+++ ntopng.sh
@@ -3,12 +3,12 @@
 #At 18:00:32 Saturday 05/03/2016			       
 
 
-echo -e "Installation de ntopng-2.4:\n"
+echo -e "Installation de ntopng-2.4-stable:\n"
 
 
-wget http://downloads.sourceforge.net/project/ntop/ntopng/ntopng-2.4.tar.gz
-tar -zxvf ntopng-2.4.tar.gz
-mv ntopng-2.4 /opt/ntopng
+wget http://downloads.sourceforge.net/project/ntop/ntopng/ntopng-2.4-stable.tar.gz
+tar -zxvf ntopng-2.4-stable.tar.gz
+mv ntopng-2.4-stable /opt/ntopng
 cd /opt/ntopng
 echo -e "Installation des dépendances:\n(Press any key to continue)"
 read ok
